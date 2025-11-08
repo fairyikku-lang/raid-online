41d269b1aeab8554d495b1b8ab90f2d7e860a9da
fairyikku-lang <fairyikku@gmail.com>
2025-11-08 11:04:56 +0100

    Update supabaseServerClient.ts

--- lib/supabaseServerClient.ts
+++ lib/supabaseServerClient.ts
@@ -1,5 +1,5 @@
 // lib/supabaseServerClient.ts
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
 export function createServerSupabaseClient() {
@@ -12,6 +12,20 @@ export function createServerSupabaseClient() {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({
+            name,
+            value,
+            ...options,
+          })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({
+            name,
+            value: '',
+            ...options,
+          })
         },
       },
     }
