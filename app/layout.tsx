bb033ecb3706d2e60091aa529d5e1df88299e0fd
fairyikku-lang <fairyikku@gmail.com>
2025-11-11 22:10:09 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import './styles/hero.css';
 export const metadata = { title: 'RAID Online v1', description: 'Heroes & Gear' };
 export default function RootLayout({ children }:{children:React.ReactNode}){
   return (<html lang="pl"><body>{children}</body></html>);
